78f10b1dce9226b3786f350836ae4654c3d33d37
sm3131 <sm2683@nau.edu>
2022-03-24 16:32:17 -0700

    added links to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,9 +7,9 @@ function Footer() {
             <p>Thanks for Visiting!</p>
             </div>
             <div className="row">
-                <a className="col-12 col-md-4 col-lg-4 footer-links">GitHub</a>
-                <a className="col-12 col-md-4 col-lg-4 footer-links">LinkedIn</a>
-                <a className="col-12 col-md-4 col-lg-4 footer-links">Other</a>
+                <a href="https://github.com/sm3131" target="_blank" className="col-12 col-md-4 col-lg-4 footer-links">GitHub</a>
+                <a href="https://www.linkedin.com/in/smoore31/"  target="_blank" className="col-12 col-md-4 col-lg-4 footer-links">LinkedIn</a>
+                <a target="_blank" className="col-12 col-md-4 col-lg-4 footer-links">Other</a>
             </div>
         </footer>
     )
